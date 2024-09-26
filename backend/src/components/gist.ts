69b51c2acd06d2b5a26d44b6762adf968bf96d85
suyash <suyashmishra145.com>
2024-09-26 15:29:24 +0530

    Completing the CRUD operation routes for gists

--- backend/src/components/gist.ts
+++ backend/src/components/gist.ts
@@ -1,25 +1,130 @@
 import { Hono } from "hono";
+import auth from "../middleware/authenticate";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const gist = new Hono();
+const gist = new Hono<{
+    Bindings:
+    {
+        DATABASE_URL: string
+    }
+}>();
 
-gist.post('/',(c):any => 
+gist.post('/creategist',auth,async (c:any) => 
 {
-    return c.text('posting a gist!');
+    const prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: c.env.DATABASE_URL,  // Ensure DATABASE_URL contains the correct prisma:// URL
+          },
+        },
+      }).$extends(withAccelerate());
+
+      const {content,id} = await c.req.json();
+
+      try 
+      {
+         await prisma.gist.create({
+            //@ts-ignore
+            data: 
+            {
+                content: content
+            }
+         })  
+         
+         return c.json({
+            content: `${content}`,
+            id: `${id}`
+         });
+      }
+      catch(err) 
+      {
+        return c.text('Some error occurred',411);
+      }
 })
 
-gist.get('/getgist/:id',(c):any => 
+gist.get('/getgist/:id', async (c:any) => 
 {
-    return c.text('posting a gist!');
+    const prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: c.env.DATABASE_URL,  // Ensure DATABASE_URL contains the correct prisma:// URL
+          },
+        },
+      }).$extends(withAccelerate());
+
+    const id = parseInt(c.req.param('id'));
+
+    try 
+    {
+    const gist = await prisma.gist.findUnique({
+        where: 
+            {
+                id : id
+            }
+        })
+    
+        return c.json(gist);
+    }
+    catch 
+    {
+        return c.text('Some error occurred', 411);
+    }  
 })
 
-gist.put('/:id',(c):any => 
+gist.put('/updategist/:id',auth, async (c:any) => 
 {
-    return c.text('posting a gist!');
+    const prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: c.env.DATABASE_URL,  // Ensure DATABASE_URL contains the correct prisma:// URL
+          },
+        },
+      }).$extends(withAccelerate());
+
+    const updatedContent = await c.req.json();
+    const id = parseInt(c.req.params('id'));
+
+    try 
+    {
+        await prisma.gist.update({
+            where: 
+            {
+                id: id
+            }, 
+            data: 
+            {
+                content: updatedContent
+            }
+        })
+    }
+    catch 
+    {
+        c.text('Some error occurred', 411);
+    }
 })
 
-gist.delete('/:id',(c):any => 
+gist.delete('/delete/:id',auth,async (c:any) => 
 {
-    return c.text('posting a gist!');
+    const prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: c.env.DATABASE_URL,  // Ensure DATABASE_URL contains the correct prisma:// URL
+          },
+        },
+      }).$extends(withAccelerate());
+
+    const id = parseInt(c.req.params('id'));
+    
+    try 
+    { 
+        
+
+    }
+    catch 
+    {
+
+    }
 })
 
 export default gist;
